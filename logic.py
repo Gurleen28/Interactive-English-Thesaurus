e4c5d186e85e411549cff9d0701b7f041fdd4d1a
Gurleen Kour <kour@plan-b-bremen.de>
2021-10-14 10:09:30 +0200

    optimized output

--- logic.py
+++ logic.py
@@ -15,13 +15,13 @@ def generateDictLists():
 
 def searchWord(word, frame):
     if database.getDefinitions(word.lower()):              # most words in database are lowercase
-        return database.getDefinitions(word.lower())
+        return optimizeDefinitions(database.getDefinitions(word.lower()), frame)
     elif database.getDefinitions(word):                    # if words are written correctly (ex. correctly capitalized)
-        return database.getDefinitions(word)
+        return optimizeDefinitions(database.getDefinitions(word), frame)
     elif database.getDefinitions(word.capitalize()):       # capitals and countries
-        return database.getDefinitions(word.capitalize())
+        return optimizeDefinitions(database.getDefinitions(word.capitalize()), frame)
     elif database.getDefinitions(word.upper()):            # USA and other acronyms
-        return database.getDefinitions(word.upper())
+        return optimizeDefinitions(database.getDefinitions(word.upper()), frame)
     # add intelligent search
     elif len(get_close_matches(word, data.keys(), n=3, cutoff=0.8)) > 0:
         matches = get_close_matches(word, data.keys(), n=3, cutoff=0.8)
@@ -29,3 +29,12 @@ def searchWord(word, frame):
         return ""
     else:
         return "Word not found."
+
+def optimizeDefinitions(mydefs, frame):
+    result = ""
+    for i in range(0, len(mydefs)):
+        result += str(i + 1) + ". " + mydefs[i] + "\n"
+    if i >= 4:
+        frame.Maximize()
+    return result
+
